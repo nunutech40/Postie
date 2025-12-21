fa5a080004b279824d95709b1888bbf9c3e2c291
Nunu Nugraha <nununugraha@Nunus-MacBook-Pro.local>
2025-12-21 14:02:33 +0700

    initiate project postie

---
+++ Postie/PostieApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostieApp.swift
+//  Postie
+//
+//  Created by Nunu Nugraha on 21/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PostieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
